166903752591e00bdaeb271012e97ace0c4d5f0a
jaffe.huang <jaffe.huang@anker-in.com>
2023-11-13 15:45:48 +0800

    Fix nav-link

--- app/dashboard/nav-links.tsx
+++ app/dashboard/nav-links.tsx
@@ -1,39 +1,44 @@
 'use client';
+ 
 import {
-    UserGroupIcon,
-    HomeIcon,
-    DocumentDuplicateIcon,
-  } from '@heroicons/react/24/outline';
-import { Client } from '@vercel/postgres';
-import clsx from 'clsx';
-  import Link from 'next/link';
+  UserGroupIcon,
+  HomeIcon,
+  DocumentDuplicateIcon,
+} from '@heroicons/react/24/outline';
+import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-   
-  // ...
-   
-  export default function NavLinks() {
-    const pathname = usePathname();
-    return (
-      <>
-        {links.map((link) => {
-          const LinkIcon = link.icon;
-          return (
-            <Link
-              key={link.name}
-              href={link.href}
-            //   className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-              className={clsx(
-                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                {
-                    'bg-sky-100 text-blue-600': pathname === link.href,
-                }
-              )}
-            >
-              <LinkIcon className="w-6" />
-              <p className="hidden md:block">{link.name}</p>
-            </Link>
-          );
-        })}
-      </>
-    );
-  }
+import clsx from 'clsx';
+ 
+const links = [
+  { name: 'Home', href: '/', icon: HomeIcon },
+  { name: 'Users', href: '/users', icon: UserGroupIcon },
+  { name: 'Documents', href: '/documents', icon: DocumentDuplicateIcon },
+  // Add more links as needed
+];
+ 
+export default function NavLinks() {
+  const pathname = usePathname();
+ 
+  return (
+    <>
+      {links.map((link) => {
+        const LinkIcon = link.icon;
+        return (
+          <Link
+            key={link.name}
+            href={link.href}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-blue-600': pathname === link.href,
+              },
+            )}
+          >
+            <LinkIcon className="w-6" />
+            <p className="hidden md:block">{link.name}</p>
+          </Link>
+        );
+      })}
+    </>
+  );
+}
