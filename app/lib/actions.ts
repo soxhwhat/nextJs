6072834e28b2a62063434fefd3a664ca580c5edf
soxhwhat <1246669436@qq.com>
2023-11-24 19:13:02 +0800

    Refactor invoice creation page and form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,16 +7,27 @@ import { z } from "zod";
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer',
+    }),
+    amount: z.coerce.number().gt(0, { message: 'Amount must be greater than 0' }),
+    status: z.enum(['pending', 'paid'], { invalid_type_error: 'Please select a status' }),
     date: z.string(),
 });
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
+
+export async function createInvoice(precState: State, formData: FormData) {
     // const rawFromData = {
     //     customerId: formdata.get('customerId'),
     //     amount: formdata.get('amount'),
@@ -24,11 +35,20 @@ export async function createInvoice(formData: FormData) {
     // }
     // console.log(rawFromData);
 
-    const { customerId, amount, status } = CreateInvoice.parse({
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Failed to create invoice',
+        };
+    }
+    const { customerId, amount, status } = validatedFields.data;
+
 
     const amountIncent = amount * 100;
     const date = new Date().toISOString().split('T')[0];
