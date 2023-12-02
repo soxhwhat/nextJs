3700ce10e1a085a98f69c53b9bed2d610c8cd926
soxhwhat <1246669436@qq.com>
2023-12-02 22:38:13 +0800

    Fix dashboard card

--- app/ui/dashboard/revenue-chart.tsx
+++ app/ui/dashboard/revenue-chart.tsx
@@ -12,7 +12,14 @@ import { fetchRevenue } from '@/app/lib/data';
 // https://airbnb.io/visx/
 
 export default async function RevenueChart() {
-    const revenue = await fetchRevenue();
+  const revenue = await fetchRevenue();
+
+  const chartHeight = 350;
+  const { yAxisLabels, topLabel } = generateYAxis(revenue);
+ 
+  if (!revenue || revenue.length === 0) {
+    return <p className="mt-4 text-gray-400">No data available.</p>;
+  }
 
   return (
     <div className="w-full md:col-span-4">
@@ -23,7 +30,7 @@ export default async function RevenueChart() {
 
       <div className="rounded-xl bg-gray-50 p-4">
         <div className="sm:grid-cols-13 mt-0 grid grid-cols-12 items-end gap-2 rounded-md bg-white p-4 md:gap-4">
-          {/* <div
+          <div
             className="mb-6 hidden flex-col justify-between text-sm text-gray-400 sm:flex"
             style={{ height: `${chartHeight}px` }}
           >
@@ -44,7 +51,7 @@ export default async function RevenueChart() {
                 {month.month}
               </p>
             </div>
-          ))} */}
+          ))}
         </div>
         <div className="flex items-center pb-2 pt-6">
           <CalendarIcon className="h-5 w-5 text-gray-500" />
